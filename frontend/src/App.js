a97d748122ee4520494d6824e0c86f0add0d10e9
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-11 20:21:47 +0300

    feat(routing): Add and define routes for all pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,8 @@
 //    - /events/<some-id> => EventDetailPage
 //    - /events/new => NewEventPage
 //    - /events/<some-id>/edit => EditEventPage
+// - COMPLETED
+
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
 // 4. Add properly working links to the MainNavigation
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
@@ -22,8 +24,23 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home.js";
+import EventsPage from "./pages/Events.js";
+import EventDetailPage from "./pages/EventDetail.js";
+import NewEventPage from "./pages/NewEvent.js";
+import EditEventPage from "./pages/EditEvent.js";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/events", element: <EventsPage /> },
+  { path: "/events/:eventId", element: <EventDetailPage /> },
+  { path: "/events/new", element: <NewEventPage /> },
+  { path: "/events/:eventId/edit", element: <EditEventPage /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
