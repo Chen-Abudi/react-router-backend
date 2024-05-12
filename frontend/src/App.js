ae14946ed62bdf6a1ac7722f0b357d249c4f4317
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-12 10:43:04 +0300

    feat: Add root layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,18 +25,26 @@
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import HomePage from "./pages/Home.js";
 import EventsPage from "./pages/Events.js";
 import EventDetailPage from "./pages/EventDetail.js";
 import NewEventPage from "./pages/NewEvent.js";
 import EditEventPage from "./pages/EditEvent.js";
+import RootLayout from "./pages/Root.js";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/events", element: <EventsPage /> },
-  { path: "/events/:eventId", element: <EventDetailPage /> },
-  { path: "/events/new", element: <NewEventPage /> },
-  { path: "/events/:eventId/edit", element: <EditEventPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "events", element: <EventsPage /> },
+      { path: "events/:eventId", element: <EventDetailPage /> },
+      { path: "events/new", element: <NewEventPage /> },
+      { path: "events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
 ]);
 
 function App() {
