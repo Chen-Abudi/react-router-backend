f04bde8469254c50a194065374bc6be14ec88786
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-16 15:40:02 +0300

    feat: Add a newsletter signup

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,6 +1,45 @@
+// import { NavLink } from "react-router-dom";
+
+// import classes from "./MainNavigation.module.css";
+
+// function MainNavigation() {
+//   return (
+//     <header className={classes.header}>
+//       <nav>
+//         <ul className={classes.list}>
+//           <li>
+//             <NavLink
+//               to="/"
+//               className={({ isActive }) =>
+//                 isActive ? classes.active : undefined
+//               }
+//               end
+//             >
+//               Home
+//             </NavLink>
+//           </li>
+//           <li>
+//             <NavLink
+//               to="/events"
+//               className={({ isActive }) =>
+//                 isActive ? classes.active : undefined
+//               }
+//             >
+//               Events
+//             </NavLink>
+//           </li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
+
+// export default MainNavigation;
+
 import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup.js";
 
 function MainNavigation() {
   return (
@@ -28,8 +67,19 @@ function MainNavigation() {
               Events
             </NavLink>
           </li>
+          <li>
+            <NavLink
+              to="/newsletter"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Newsletter
+            </NavLink>
+          </li>
         </ul>
       </nav>
+      <NewsletterSignup />
     </header>
   );
 }
